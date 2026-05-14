6ac45c557e0fa75589062ea8f59b0113f82b8283
StenSOn27 <tarashevchik27@gmail.com>
2026-05-14 23:22:34 +0300

    add username and make migration

--- src/database/models.py
+++ src/database/models.py
@@ -11,6 +11,7 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, index=True)
     password_hash: Mapped[str] = mapped_column(String(255))
 
     cart: Mapped[Optional["Cart"]] = relationship(back_populates="user", uselist=False)
