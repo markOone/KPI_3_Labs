df5fedc7f887c585140308d48d4661c67af82462
StenSOn27 <tarashevchik27@gmail.com>
2026-05-16 14:32:35 +0300

    add migration for creating admin

---
+++ alembic/versions/7c2536969df1_add_initial_admin.py
@@ -0,0 +1,42 @@
+"""add_initial_admin
+
+Revision ID: 7c2536969df1
+Revises: bb7346e3457e
+Create Date: 2026-05-16 11:24:59.808561
+
+"""
+
+from typing import Sequence, Union
+from src.config.settings import settings
+from src.auth.hashing import Hasher
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision: str = "7c2536969df1"
+down_revision: Union[str, Sequence[str], None] = "bb7346e3457e"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    connection = op.get_bind()
+
+    group_id = connection.execute(
+        sa.text("SELECT id FROM user_groups WHERE name = 'Admin'")
+    ).scalar()
+
+    admin_email = settings.admin.email
+    admin_username = settings.admin.username
+    admin_password = Hasher.get_password_hash(settings.admin.password)
+
+    op.execute(sa.text(f"""
+            INSERT INTO users (email, username, password_hash, group_id)
+            VALUES ('{admin_email}', '{admin_username}', '{admin_password}', {group_id})
+            ON CONFLICT (email) DO NOTHING;
+            """))
+
+
+def downgrade() -> None:
+    op.execute(sa.text("DELETE FROM users WHERE username = 'admin'"))
