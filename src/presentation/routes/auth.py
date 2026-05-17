dd4234d4527304b45040f794ed99afbf7eaf154a
StenSOn27 <tarashevchik27@gmail.com>
2026-05-17 15:18:34 +0300

    refactor user endpoints to follow cqs

--- src/presentation/routes/auth.py
+++ src/presentation/routes/auth.py
@@ -1,28 +1,44 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
+from src.application.commands.user_commands import AddUserCommand, LoginUserCommand
+from src.application.commands.user_handlers import (
+    AddUserCommandHandler,
+    LoginUserCommandHandler,
+)
+from src.application.queries.user_handlers import GetUserQueryHandler
+from src.application.queries.user_queries import GetUserQuery
+from src.domain.repositories.repositories import UserRepository
 from src.database.engine import db_helper
 from src.infrastructure.repositories.user_repository import UserRepositoryImpl
 from src.application.use_cases.auth_use_cases import RegisterUserUseCase
-from src.domain.errors.domain_errors import EmailAlreadyExistsError, UsernameAlreadyExistsError
-from src.config.dependencies import get_current_user, get_jwt_manager
-from src.schemas.auth import TokenResponse, UserRegisterSchema, UserResponseSchema
-from src.auth.hashing import Hasher
-from src.database.models import User as ORMUser
+from src.domain.errors.domain_errors import (
+    CredentialsError,
+    EmailAlreadyExistsError,
+    UsernameAlreadyExistsError,
+)
+from src.config.dependencies import (
+    get_current_user,
+    get_jwt_manager,
+    get_user_repository,
+)
+from src.schemas.auth import TokenResponse, UserResponseSchema
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-async def get_user_repository(db: AsyncSession = Depends(db_helper.get_db_session)):
-    return UserRepositoryImpl(db)
 
 @router.post("/register", status_code=status.HTTP_201_CREATED)
-async def register(data: UserRegisterSchema, user_repo: UserRepositoryImpl = Depends(get_user_repository)):
-    use_case = RegisterUserUseCase(user_repo)
+async def register(
+    command: AddUserCommand,
+    user_repo: UserRepositoryImpl = Depends(get_user_repository),
+):
+    handler = AddUserCommandHandler(user_repo)
     try:
-        await use_case.execute(username=data.username, email=data.email, password_plain=data.password)
-        return {"status": "success"}
+        user_id = await handler.handle(command)
+        return {"id": user_id}
     except (EmailAlreadyExistsError, UsernameAlreadyExistsError) as e:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
+
 
 @router.post("/login", response_model=TokenResponse)
 async def login(
@@ -30,17 +46,36 @@ async def login(
     user_repo: UserRepositoryImpl = Depends(get_user_repository),
     jwt_manager=Depends(get_jwt_manager),
 ):
-    user = await user_repo.get_by_username(data.username)
-    if not user or not Hasher.verify_password(data.password, user.password_hash):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
-    
-    payload = {"sub": str(user.id), "username": user.username}
+    command = LoginUserCommand(username=data.username, password=data.password)
+    handler = LoginUserCommandHandler(user_repo)
+
+    try:
+        user = await handler.handle(command)
+    except CredentialsError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e),
+        )
+
+    except Exception as e:
+        print(f"Unexpected error during login: {type(e).__name__}: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Server error",
+        )
+
+    payload = {
+        "sub": str(user.id),
+        "username": user.username,
+    }
+
     return {
         "access_token": jwt_manager.create_access_token(data=payload),
         "refresh_token": jwt_manager.create_refresh_token(data=payload),
         "token_type": "bearer",
     }
 
+
 @router.get("/me", response_model=UserResponseSchema)
-async def read_users_me(current_user: ORMUser = Depends(get_current_user)):
-    return current_user
+async def get_me(user: int = Depends(get_current_user)):
+    return user
